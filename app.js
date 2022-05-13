4be0f690a4a59788eb0bff97a3dc5750480ecdef
afeesvendy <101556554+afeesvendy@users.noreply.github.com>
2022-05-13 04:08:19 +0100

    closes #6 implemented middlewear and install morgan

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express =  require('express');
 const fs = require('fs');
+const morgan = require('morgan');
+
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/data/tours-simple.json`))
 
@@ -7,9 +9,15 @@ const app = express();
 
 app.use(express.json());
 
+app.use(morgan('dev'));
+
+app.use((req, res, next)=>{
+   console.log("this is a middleware call 😀")
+    next();
+})
 
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
 
     res.status(200)
     .json({
@@ -17,45 +25,14 @@ app.get('/api/v1/tours', (req, res) => {
         results: tours.length,
         tours
     })
-})
+}
 
-app.post('/api/v1/tours', (req, res) =>{
-
-const tourID = tours[tours.length - 1].id+1;
-
-const newTour = Object.assign({id:tourID}, req.body);
-
-tours.push(newTour);
-
-fs.writeFile(`${__dirname}/data/tours-simple.json`, JSON.stringify(tours), err => {
-res.status(201)
-.json({
-    status:'success',
-    data:{
-        tours : newTour
-    }
-})
-})
-})
-
-
-app.get(`/api/v1/tours/:id`, (req, res) => {
+const getTour = (req, res) => {
     
 
     const id  = Number(req.params.id);
 
-    if(id > tours.length){
-
-        return res.status(400)
-        .json({
-            status : 'failed',
-            message : 'Invalid id specified'
-        })
-    }
-
-    const tour = tours.find(el => el.id === id);
-
-    // if(!tour){
+    // if(id > tours.length){
 
     //     return res.status(400)
     //     .json({
@@ -63,6 +40,17 @@ app.get(`/api/v1/tours/:id`, (req, res) => {
     //         message : 'Invalid id specified'
     //     })
     // }
+
+    const tour = tours.find(el => el.id === id);
+
+    if(!tour){
+
+        return res.status(400)
+        .json({
+            status : 'failed',
+            message : 'Invalid id specified'
+        })
+    }
 
     res.status(200)
     .json({
@@ -72,7 +60,37 @@ app.get(`/api/v1/tours/:id`, (req, res) => {
     }
          
     })
-})
+}
+
+const addTour =  (req, res) => {
+
+    const tourID = tours[tours.length - 1].id+1;
+    
+    const newTour = Object.assign({id:tourID}, req.body);
+    
+    tours.push(newTour);
+    
+    fs.writeFile(`${__dirname}/data/tours-simple.json`, JSON.stringify(tours), err => {
+    res.status(201)
+    .json({
+        status:'success',
+        data:{
+            tours : newTour
+        }
+    })
+    })
+ }
+
+ app.route('/api/v1/tours').get(getAllTours).post(addTour);
+
+ app.route('/api/v1/tours/:id').get(getTour);
+
+// app.get('/api/v1/tours', getAllTours);
+
+// app.post('/api/v1/tours', addTour);
+
+//app.get(`/api/v1/tours/:id`, getTour)
+
 
 const port = 3000;
 
