e0efd555bee4d322e94342a1870490ca5704ef99
afeesvendy <101556554+afeesvendy@users.noreply.github.com>
2022-05-15 04:31:26 +0100

    create a new branch

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express =  require('express');
 const fs = require('fs');
 const morgan = require('morgan');
+const tourController = require('./controller/tourController');
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/data/tours-simple.json`));
 
@@ -21,95 +22,13 @@ app.use((req, res, next) => {
 })
 
 
-
-const getAllTours = (req, res) => {
-
-    res.status(200)
-    .json({
-        requestAt:req.requestTime,
-        status:"success",
-        results: tours.length,
-        tours
-    })
-}
-
-const getTour = (req, res) => {
-    
-
-    const id  = Number(req.params.id);
-
-    // if(id > tours.length){
-
-    //     return res.status(400)
-    //     .json({
-    //         status : 'failed',
-    //         message : 'Invalid id specified'
-    //     })
-    // }
-
-    const tour = tours.find(el => el.id === id);
-
-    if(!tour){
-
-        return res.status(400)
-        .json({
-            status : 'failed',
-            message : 'Invalid id specified'
-        })
-    }
-
-    res.status(200)
-    .json({
-        status:'success',
-        data:{
-         tour
-    }
-         
-    })
-}
-const addTour =  (req, res) => {
-
-    const tourID = tours[tours.length - 1].id+1;
-    
-    const newTour = Object.assign({id:tourID}, req.body);
-    
-    tours.push(newTour);
-    
-    fs.writeFile(`${__dirname}/data/tours-simple.json`, JSON.stringify(tours), err => {
-    res.status(201)
-    .json({
-        status:'success',
-        data:{
-            tours : newTour
-        }
-    })
-    })
- }
-
- const updateTour = (req, res) => {
-
-    res.status(500)
-    .json({
-     status:'failed',
-     massage:'updateApi still in progress'
-    })
- }
-
- const deleteTour = (req, res) => {
-    res.status(500)
-    .json({
-     status:'failed',
-     massage:'deleteApi still in progress'
-    })
- }
-
  const tourRouter = express.Router();
 
  app.use('/api/v1/tours', tourRouter)
 
- tourRouter.route('/').get(getAllTours).post(addTour);
+ tourRouter.route('/').get(tourController.getAllTours).post(tourController.addTour);
 
- tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+ tourRouter.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
 
 // app.get('/api/v1/tours', getAllTours);
